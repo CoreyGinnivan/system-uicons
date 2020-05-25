5121f1ab8a6f9cf79f8e5da1fb4e637e525d3b2d
Corey <corey@ginnivan.net>
2020-05-25 18:01:32 +0800

    Added eleventy + tailwind boilerplate

---
+++ README.md
@@ -0,0 +1,35 @@
+# Tailwind CSS Webpack Starter Project
+
+This is an example of a super simple Webpack setup for using [Tailwind CSS](https://tailwindcss.com).
+
+To get started, clone the project and install the dependencies:
+
+```
+# Using npm
+npm install
+
+# Using Yarn
+yarn
+```
+
+After that, start up Webpack Development Server:
+
+```
+npm run dev
+```
+
+Webpack Development Server will watch `/src/styles.css` and `/tailwind.js` and rebuild your stylesheet on every change.
+
+You can play around with `/index.html` to see the effects of your changes.
+
+To build a production bundle run:
+
+```
+npm run prod
+```
+
+After that you will have a ready to deploy bundle at `/dist`
+
+## Contributing
+
+Have a lot of experience with Webpack and suggestions on how we could improve this starter template? We'd love a PR!
---
+++ webpack.config.js
@@ -0,0 +1,61 @@
+const path = require("path")
+const glob = require("glob-all")
+const ExtractTextPlugin = require("extract-text-webpack-plugin")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+const CopyWebpackPlugin = require("copy-webpack-plugin")
+const PurgecssPlugin = require("purgecss-webpack-plugin")
+
+/**
+ * Custom PurgeCSS Extractor
+ * https://github.com/FullHuman/purgecss
+ * https://github.com/FullHuman/purgecss-webpack-plugin
+ */
+class TailwindExtractor {
+  static extract(content) {
+    return content.match(/[A-z0-9-:\/]+/g)
+  }
+}
+
+module.exports = {
+  entry: "./src/css/tailwind.src.css",
+  mode: process.env.NODE_ENV,
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          use: [
+            { loader: "css-loader", options: { importLoaders: 1 } },
+            "postcss-loader",
+          ],
+        }),
+      },
+    ],
+  },
+  plugins: [
+    new ExtractTextPlugin("styles.css", {
+      disable: process.env.NODE_ENV === "development",
+    }),
+    new HtmlWebpackPlugin({
+      filename: "index.html",
+      template: "src/index.html",
+    }),
+    /// Enable images folder
+    // new CopyWebpackPlugin([
+    //   {
+    //     from: "src/images",
+    //     to: "images",
+    //   },
+    // ]),
+    new PurgecssPlugin({
+      paths: glob.sync([path.join(__dirname, "src/**/*.html")]),
+      extractors: [
+        {
+          extractor: TailwindExtractor,
+          extensions: ["html", "js"],
+        },
+      ],
+    }),
+  ],
+}
