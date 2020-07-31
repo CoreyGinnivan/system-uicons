451cce964ebe07540f0f39a1daf61a0a4159f58b
Corey <corey@ginnivan.net>
2020-07-31 10:32:54 +0800

    Moved icons, setup download all button

---
+++ createZip.js
@@ -0,0 +1,28 @@
+var fs = require("fs");
+var archiver = require("archiver");
+
+function zipDirectory(sourcePath, zipPath) {
+  var zipStream = fs.createWriteStream(zipPath);
+
+  var archive = archiver("zip");
+  archive.on("warning", (err) => {
+    if (err.code === "ENOENT") {
+      console.log(err);
+    } else {
+      throw err;
+    }
+  });
+  archive.on("error", (err) => {
+    throw err;
+  });
+  archive.pipe(zipStream);
+  archive.directory(sourcePath, false);
+  archive.finalize();
+}
+
+var sourcePath = __dirname + "/src/images/icons/";
+var zipPath = __dirname + "/src/images/icons.zip";
+
+console.log(`Zipping ${sourcePath} to ${zipPath} (overwrites existing)`);
+
+zipDirectory(sourcePath, zipPath);
