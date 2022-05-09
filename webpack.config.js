bc905d3ca228e56811903c5fe249f23b57afa126
Corey Ginnivan <corey@ginnivan.net>
2022-05-09 19:01:19 +0800

    Tweaks

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,7 @@
-const path = require("path");
-const glob = require("glob-all");
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
-const PurgecssPlugin = require("purgecss-webpack-plugin");
 
-/**
- * Custom PurgeCSS Extractor
- * https://github.com/FullHuman/purgecss
- * https://github.com/FullHuman/purgecss-webpack-plugin
- */
-class TailwindExtractor {
-  static extract(content) {
-    return content.match(/[A-z0-9-:\/]+/g);
-  }
-}
 
 module.exports = {
   entry: "./src/css/tailwind.src.css",
@@ -26,7 +13,7 @@ module.exports = {
         use: ExtractTextPlugin.extract({
           fallback: "style-loader",
           use: [
-            { loader: "css-loader", options: { importLoaders: 1 } },
+            { loader: "css-loader", options: { importLoaders: 2 } },
             "postcss-loader",
           ],
         }),
@@ -55,14 +42,5 @@ module.exports = {
         to: "js",
       },
     ]),
-    new PurgecssPlugin({
-      paths: glob.sync([path.join(__dirname, "src/**/*.html")]),
-      extractors: [
-        {
-          extractor: TailwindExtractor,
-          extensions: ["html", "js"],
-        },
-      ],
-    }),
   ],
 };
