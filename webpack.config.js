116abe5cd12cca8da489b81268ac30022ce1fcd9
Jake Ginnivan <jake@ginnivan.net>
2022-05-09 20:27:15 +0800

    Upgrade all the things

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,6 @@
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
-
 
 module.exports = {
   entry: "./src/css/tailwind.src.css",
@@ -10,37 +9,28 @@ module.exports = {
     rules: [
       {
         test: /\.css$/,
-        use: ExtractTextPlugin.extract({
-          fallback: "style-loader",
-          use: [
-            { loader: "css-loader", options: { importLoaders: 2 } },
-            "postcss-loader",
-          ],
-        }),
+        use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader"],
       },
     ],
   },
   plugins: [
-    new ExtractTextPlugin("styles.css", {
-      disable: process.env.NODE_ENV === "development",
-    }),
+    new MiniCssExtractPlugin(),
     new HtmlWebpackPlugin({
       filename: "index.html",
       template: "src/index.html",
     }),
     /// Enable images folder
-    new CopyWebpackPlugin([
-      {
-        from: "src/images",
-        to: "images",
-      },
-    ]),
-    /// Enable js folder
-    new CopyWebpackPlugin([
-      {
-        from: "src/js",
-        to: "js",
-      },
-    ]),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: "src/images",
+          to: "images",
+        },
+        {
+          from: "src/js",
+          to: "js",
+        },
+      ],
+    }),
   ],
 };
